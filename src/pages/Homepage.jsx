8f68aacb4d6e345fd32db2fc200bc94ebcf0d8db
ismail <ismaillakkca@gmail.com>
2025-07-13 23:35:05 +0300

    fix: correct typo in Homepage heading

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -8,7 +8,7 @@ export default function Homepage() {
       <PageNav />
       <section>
         <h1>
-          You travel the world.
+          You travel the worldd.
           <br />
           WorldWise keeps track of your adventures.
         </h1>
